223b1dfa277a3a7e335b8cde5a60a858c11f6eb9
Jonathan <jonathan.gabioud@hotmail.com>
2024-01-17 20:33:29 +0100

    Prepare Orbit class with draw method

---
+++ orbit.py
@@ -0,0 +1,11 @@
+import pygame
+
+class Orbit:
+    def __init__(self, x, y, color, screen):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.screen = screen
+        
+    def draw(self):
+        pygame.draw.ellipse(self.screen, self.color, (400-(self.x/2), 400-(self.y/2), self.x, self.y), 100, 1)
