deb83352307878e87462b7e0ce19d3a65d380826
Jonathan <jonathan.gabioud@hotmail.com>
2024-01-17 20:29:48 +0100

    Prepare PLanet method for movement and add missing attributes

--- planet.py
+++ planet.py
@@ -1,7 +1,11 @@
+import pygame, sys, math
+
 class Planet:
-    def __init__(self, x: int, y: int, radius: int, color: tuple, screen: object, angle_increase: int, width: int, height: int):
+    def __init__(self, x: int, y: int, ratio_x: int, ratio_y: int, radius: int, color: tuple, screen: object, angle_increase: int, width: int, height: int):
         self.x = x
         self.y = y
+        self.ratio_x = ratio_x
+        self.ratio_y = ratio_y
         self.radius = radius
         self.color = color
         self.screen = screen
@@ -9,3 +13,9 @@ class Planet:
         self.width = width
         self.height = height
         self.angle = 0
+        
+    def move(self):
+        self.y = int(math.cos(self.angle) * self.ratio_x) + self.width / 2
+        self.x = (int(math.sin(self.angle) * self.ratio_y) + self.height / 2)
+        pygame.draw.circle(self.screen, self.color, (self.x, self.y), self.radius)
+        self.angle -= self.angle_increase
