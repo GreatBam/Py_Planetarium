e9405da97777e24c3802f07f52299f864f0c1adc
Jonathan <jonathan.gabioud@hotmail.com>
2024-01-17 20:23:57 +0100

    Set some colors

--- main.py
+++ main.py
@@ -10,3 +10,12 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Planetarium")
 running = True
 
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREY = (150, 150, 150)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+PURPLE = (128, 0, 128)
+
