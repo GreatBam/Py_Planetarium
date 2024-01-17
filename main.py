78ae14cff95b48ba5b351ba70ebb342bf44cc673
Jonathan <jonathan.gabioud@hotmail.com>
2024-01-17 20:23:04 +0100

    Set game variables

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 # Planetarium
+
+import pygame
+
+WIDTH = 800
+HEIGHT = 800
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Planetarium")
+running = True
+
