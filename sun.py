4e6d2e759d79e2ad82d2ea602cd9c027049420aa
Jonathan <jonathan.gabioud@hotmail.com>
2024-01-17 20:30:42 +0100

    Prepare Sun class and attributes

---
+++ sun.py
@@ -0,0 +1,7 @@
+class Sun:
+    def __init__(self, x, y, radius, color, screen):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.screen = screen
