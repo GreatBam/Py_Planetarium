cf91b8acef929ecae8baee105fd2cbcc2c50bf6c
Jonathan <jonathan.gabioud@hotmail.com>
2024-01-17 20:35:57 +0100

    import all classes in main and set clock

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 # Planetarium
 
+# Imports
 import pygame
+from sun import Sun
+from planet import Planet
+from orbit import Orbit
 
+# Constants
 WIDTH = 800
 HEIGHT = 800
-
-pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Planetarium")
-running = True
 
 # Colors
 BLACK = (0, 0, 0)
@@ -19,3 +19,11 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 PURPLE = (128, 0, 128)
 
+# Pygame setup
+pygame.init()
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Planetarium")
+running = True
+
+# Classes
